f0eb70fbaaec71b3afa098135926a9ca569d1ebb
tmyg <cybmbs315@gmail.com>
2025-05-01 17:54:09 +0900

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ export const App=()=> {
  const onClickSwtich=()=>setIsAdmin(!isAdmin);
   return (
    <div>
+    <h1>AWSテストサイト</h1>
     {isAdmin?<span>管理者です</span>:<span>管理者以外です</span>}
     <button onClick={onClickSwtich}>切り替え</button>
     <Card isAdmin={isAdmin}/>
