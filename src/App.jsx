437f806088a900f421c239a030fbe37a1105da95
tmyg <cybmbs315@gmail.com>
2025-05-01 17:46:22 +0900

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+import{ Card } from "./components/Card"
+
+export const App=()=> {
+  const {isAdmin,setIsAdmin}=useState(false);
+ const onClickSwtich=()=>setIsAdmin(!isAdmin);
+  return (
+   <div>
+    {isAdmin?<span>管理者です</span>:<span>管理者以外です</span>}
+    <button onClick={onClickSwtich}>切り替え</button>
+    <Card isAdmin={isAdmin}/>
+   </div>
+  );
+}
+
+
